c37b754b687c55656114c852b7b7672d951edf23
Marina Baskova <marinabaskova24@gmail.com>
2019-07-18 12:52:31 -0700

    added eat cokkie functions

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,8 +5,47 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
+
+# def eating_cookies(n, cache=None):
+  
+#   if n < 0:
+#     return 0
+#   if n == 0:
+#     return 1
+#   if n == 1:
+#     return 1
+#   if n == 2:
+#     return 2
+#   return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)  
+
 def eating_cookies(n, cache=None):
-  pass
+
+  cache = {}
+  result = None
+
+  if n in cache:
+    return cache[n]
+
+  if n < 0:
+    result = 0
+
+  if n == 0:
+    result = 1
+
+  if n == 1:
+    result = 1
+
+  if n == 2:
+    result = 2
+
+  if n > 2:
+    result = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)  
+    cache[n] = result
+
+  return result
+
+
+
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
