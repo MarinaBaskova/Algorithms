cc10eb1c1ba01f15185de735845f6f9b017a5435
Marina Baskova <marinabaskova24@gmail.com>
2019-07-21 12:51:12 -0700

    practice algorithms

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,17 +6,17 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 
-# def eating_cookies(n, cache=None):
+def eating_cookies(n, cache=None):
   
-#   if n < 0:
-#     return 0
-#   if n == 0:
-#     return 1
-#   if n == 1:
-#     return 1
-#   if n == 2:
-#     return 2
-#   return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)  
+  if n < 0:
+    return 0
+  if n == 0:
+    return 1
+  if n == 1:
+    return 1
+  if n == 2:
+    return 2
+  return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)  
 
 def eating_cookies(n, cache=None):
 
@@ -39,7 +39,7 @@ def eating_cookies(n, cache=None):
     result = 2
 
   if n > 2:
-    result = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)  
+    result = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache) 
     cache[n] = result
 
   return result
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -31,6 +31,18 @@ def find_max_profit(prices):
     return max_difference
 
 
+def find_max_profit(prices):
+    max_profit_so_far = 0
+
+    for i in range(len(prices)):
+        for j in range(i+1,len(prices)):
+            current_profit_price = prices[j] - prices[i]
+            if current_profit_price > max_profit_so_far:
+                max_profit_so_far = current_profit_price
+    return max_profit_so_far            
+
+print(find_max_profit([1050, 270, 1540, 3800, 2]))
+
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
   parser = argparse.ArgumentParser(description='Find max profit from prices.')
